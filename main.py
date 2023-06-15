2322224dbb3497d6592f17318a1b016d4910523c
Eduardo Gutiérrez <72224749+Eduardo-318@users.noreply.github.com>
2023-06-15 16:26:55 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from Cosas import Libro, Autor,Alumno
+
+def main():
+    l1= Libro.libro_planeta("El perfume", Autor("Patrik", "PS"), 1980)
+    print(l1)
+    l1.autor.pseudonimo = l1.autor.pseudonimo.lower()
+    print(l1)
+    print("------ Herencia------")
+    al2=Alumno("José",19,"32421","ICO",9)
+    al2.nombre="Pepe"
+    print(vars(al2))
+main()
